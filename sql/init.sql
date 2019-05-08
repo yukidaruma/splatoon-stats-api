2a917c57fd9bc6e65b19f4d9a66d375c442c2b20
yukidaruma <yukidaruma@users.noreply.github.com>
2019-05-08 18:57:49 +0900

    Make sure postgres and knex uses UTC for timestamps

--- sql/init.sql
+++ sql/init.sql
@@ -1,5 +1,8 @@
 CREATE ROLE readonly PASSWORD 'readonly';
 REVOKE CREATE ON SCHEMA public FROM public;
-GRANT CREATE ON SCHEMA public TO postgres;
+GRANT CREATE ON SCHEMA public TO sranking;
 REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM readonly;
 ALTER ROLE "readonly" WITH LOGIN;
+
+ALTER DATABASE sranking SET timezone TO 'UTC';
+SELECT pg_reload_conf();
--- src/db.js
+++ src/db.js
@@ -4,14 +4,23 @@ const config = require('../config');
 
 pg.types.setTypeParser(20, val => (val === null ? null : parseInt(val, 10))); // int
 pg.types.setTypeParser(1700, val => (val === null ? null : parseFloat(val, 10))); // numeric
+pg.types.setTypeParser(1114, val => val); // timestamp
+pg.types.setTypeParser(1184, val => val); // timestamptz
+
 console.log('connecting %s', (config.NODE_ENV === 'test' ? config.POSTGRES_TEST_URL : config.POSTGRES_URL));
 const db = knex({
   client: 'pg',
   connection: config.POSTGRES_URL,
   pool: {
     afterCreate(conn, done) {
-      console.log('Successfully connected to database.');
-      done();
+      conn.query("SET TIME ZONE 'UTC'", (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Successfully connected to database.');
+        }
+        done(err, conn);
+      });
     },
   },
 });
