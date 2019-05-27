bf06dff1d81de64e9ede0ecd81d76c487a9e28f7
yukidaruma <yukidaruma@users.noreply.github.com>
2019-05-27 09:26:43 +0900

    Refactor queryWeaponRanking

--- src/query.js
+++ src/query.js
@@ -3,120 +3,93 @@ const { db } = require('./db');
 // Note that you may need to add player_names.player_name in select clause.
 const joinLatestName = tableName => db.raw('latest_player_names_mv as player_names on player_names.player_id = :tableName:.player_id', { tableName });
 
-const queryWeaponRanking = (rankingType, weaponType, startTime, endTime, ruleId) => new Promise((resolve, reject) => {
+const queryWeaponRanking = args => new Promise((resolve, reject) => {
+  const {
+    rankingType, weaponType, startTime, endTime, ruleId,
+  } = args;
+
   const tableName = `${rankingType}_rankings`;
+  let statements;
 
   if (weaponType === 'weapons') {
-    db.raw(`
-      with popular_weapons as (
-        select
-            -- Group identical weapons (e.g. Hero Shot Replica and Splattershot)
-            case
-              when weapons.reskin_of is NOT NULL then weapons.reskin_of
-              else :tableName:.weapon_id
-            end as temp_weapon_id,
-            count(:tableName:.weapon_id),
-            sub_weapon_id,
-            special_weapon_id
-        from :tableName:
-          inner join weapons on :tableName:.weapon_id = weapons.weapon_id
-          ${rankingType !== 'league' ? '-- ' : ''}inner join league_schedules on :tableName:.start_time = league_schedules.start_time
-          where
-            (
-              :ruleId = 0
-              OR
-              :ruleId = ${rankingType === 'league' ? 'league_schedules' : ':tableName:'}.rule_id
-            )
-            AND
-            (
-              :startTime <= :tableName:.start_time
-              AND
-              :tableName:.start_time < :endTime
-            )
-          group by temp_weapon_id, sub_weapon_id, special_weapon_id
-          order by count desc, temp_weapon_id desc
-      )
-      select
-          RANK() over (order by popular_weapons.count desc),
-          count,
-          popular_weapons.temp_weapon_id as weapon_id,
-          sub_weapon_id,
-          special_weapon_id,
-          100 * count / sum(count) over () as percentage
-        from popular_weapons`,
-    {
-      tableName, startTime, endTime, ruleId,
-    })
-      .then(result => resolve(result.rows))
-      .catch(err => reject(err));
+    statements = {
+      select: `
+      -- Group identical weapons (e.g. Hero Shot Replica and Splattershot)
+      case
+        when weapons.reskin_of is NOT NULL then weapons.reskin_of
+        else :tableName:.weapon_id
+      end as temp_weapon_id,
+      count(:tableName:.weapon_id),
+      sub_weapon_id,
+      special_weapon_id`,
+      groupBy: ['temp_weapon_id', 'sub_weapon_id', 'special_weapon_id'],
+      orderBy: ['temp_weapon_id', 'desc'],
+      columns: [
+        'popular_weapons.temp_weapon_id as weapon_id',
+        'sub_weapon_id',
+        'special_weapon_id',
+      ],
+    };
   } else if (weaponType === 'mains') {
-    db.raw(`
-    with main_weapon_ranking as (
-      select main_reference as weapon_id, count(*) from :tableName:
-        inner join weapons on weapons.weapon_id = :tableName:.weapon_id
-        ${rankingType !== 'league' ? '-- ' : ''}inner join league_schedules on :tableName:.start_time = league_schedules.start_time
-        where
-          (
-            :ruleId = 0 or
-            :ruleId = ${rankingType === 'league' ? 'league_schedules' : ':tableName:'}.rule_id
-          )
-          and
-          (
-            :startTime <= :tableName:.start_time and
-            :tableName:.start_time < :endTime
-          )
-        group by main_reference
-        order by count desc
-    )
-    select
-        *,
-        rank () over (order by count desc),
-        100 * count / sum(count) over () as percentage
-      from main_weapon_ranking`, {
-      tableName, startTime, endTime, ruleId,
-    })
-      .then(result => resolve(result.rows))
-      .catch(err => reject(err));
+    statements = {
+      select: 'main_reference as main_weapon_id, count(*)',
+      groupBy: ['main_weapon_id'],
+      columns: ['main_weapon_id as weapon_id'],
+    };
   } else if (['specials', 'subs'].includes(weaponType)) {
     // e.g.) specials -> special_weapon_id
     const weaponTypeColumnName = `${weaponType.substring(0, weaponType.length - 1)}_weapon_id`;
-    db.raw(`
-      with popular_weapons as (
-        select
-            count(weapons.:weaponTypeColumnName:),
-            weapons.:weaponTypeColumnName:
-          from :tableName:
-            inner join weapons on :tableName:.weapon_id = weapons.weapon_id
-            ${rankingType !== 'league' ? '-- ' : ''}inner join league_schedules on :tableName:.start_time = league_schedules.start_time
-            WHERE
-              (
-                :ruleId = 0
-                OR
-                :ruleId = ${rankingType === 'league' ? 'league_schedules' : tableName}.rule_id
-              )
-              AND
-              (
-                :startTime <= :tableName:.start_time
-                AND
-                :tableName:.start_time < :endTime
-              )
-            group by weapons.:weaponTypeColumnName:
-            order by count desc, weapons.:weaponTypeColumnName: desc
-      )
-      select
-        rank() over (order by popular_weapons.count desc),
-        :weaponTypeColumnName:,
-        count,
-        100 * count / sum(count) over () as percentage
-      from popular_weapons`,
-    {
-      tableName, weaponTypeColumnName, startTime, endTime, ruleId,
-    })
-      .then(result => resolve(result.rows))
-      .catch(err => reject(err));
+
+    statements = {
+      select: `count(weapons.${weaponTypeColumnName}), weapons.${weaponTypeColumnName}`,
+      groupBy: [`weapons.${weaponTypeColumnName}`],
+      orderBy: [`weapons.${weaponTypeColumnName}`, 'desc'],
+      columns: [weaponTypeColumnName],
+    };
   } else { // Theoretically this code is unreachable
     reject(new Error('Wrong weaponType'));
   }
+
+  const popularWeaponsQuery = db.with(
+    'popular_weapons',
+    function subquery() {
+      this
+        .select(db.raw(statements.select, { tableName }))
+        .from(tableName)
+        .innerJoin('weapons', `${tableName}.weapon_id`, 'weapons.weapon_id');
+
+      if (rankingType === 'league') {
+        this.innerJoin('league_schedules', `${tableName}.start_time`, 'league_schedules.start_time');
+      }
+
+      this
+        .where(function whereRuleId() {
+          this.where(0, ruleId)
+            .orWhere(`${rankingType === 'league' ? 'league_schedules' : tableName}.rule_id`, ruleId);
+        })
+        .andWhere(function whereStartTime() {
+          this.where(`${tableName}.start_time`, '>=', startTime)
+            .andWhere(`${tableName}.start_time`, '<=', endTime);
+        })
+        .groupBy(...statements.groupBy)
+        .orderBy('count', 'desc');
+
+      if (statements.orderBy) {
+        this.orderBy(...statements.orderBy);
+      }
+    },
+  );
+
+  popularWeaponsQuery
+    .select(
+      ...statements.columns,
+      'count',
+      db.raw('rank () over (order by count desc)'),
+      db.raw('100 * count / sum(count) over () as percentage'),
+    )
+    .from('popular_weapons')
+    .then(result => resolve(result))
+    .catch(err => reject(err));
 });
 
 const queryWeaponTopPlayers = () => db.raw(`
