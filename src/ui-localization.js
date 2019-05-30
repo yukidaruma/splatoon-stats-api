03617ded66be8e356e68a3148a90e60618531a2d
yukidaruma <yukidaruma@users.noreply.github.com>
2019-05-30 09:24:19 +0900

    Add rule_shortnames

--- src/ui-localization.js
+++ src/ui-localization.js
@@ -1,9 +1,21 @@
 module.exports = {
   en: {
     bomb_launchers_combined: 'Bomb launchers combined',
+    rule_shortnames: {
+      splat_zones: 'SZ',
+      tower_control: 'TC',
+      rainmaker: 'RM',
+      clam_blitz: 'CB',
+    },
   },
 
   ja: {
     bomb_launchers_combined: 'ボムピッチャー合計',
+    rule_shortnames: {
+      splat_zones: 'エリア',
+      tower_control: 'ヤグラ',
+      rainmaker: 'ホコ',
+      clam_blitz: 'アサリ',
+    },
   },
 };
