25ee689164b12c90a301fc6431ea9e821c5c23e6
yukidaruma <yukidaruma@users.noreply.github.com>
2019-06-09 19:20:48 +0900

    Update indentation

--- src/populate_database.js
+++ src/populate_database.js
@@ -73,138 +73,136 @@ const addWeapons = (statInkWeapons) => {
 };
 
 const populateDatabase = (statInkWeapons) => {
-  async.series(
-    {
-      _addWeaponClasses(next) {
-        const tasks = weaponClasses.map(weaponClass => db.raw(
-          'INSERT INTO weapon_classes (weapon_class_id, weapon_class_key) VALUES (?, ?) ON CONFLICT DO NOTHING',
-          [weaponClass.id, weaponClass.key],
-        ));
+  async.series({
+    _addWeaponClasses(next) {
+      const tasks = weaponClasses.map(weaponClass => db.raw(
+        'INSERT INTO weapon_classes (weapon_class_id, weapon_class_key) VALUES (?, ?) ON CONFLICT DO NOTHING',
+        [weaponClass.id, weaponClass.key],
+      ));
 
-        Promise.all(tasks)
-          .then(() => {
-            next();
-            console.log('_addWeaponClasses is successfully done.');
-          })
-          .catch(err => next(err));
-      },
-      _addSpecialWeapons(next) {
-        const tasks = specialWeapons.map(specialWeapon => db.raw(
-          'INSERT INTO special_weapons (special_weapon_id, special_weapon_key) VALUES (?, ?) ON CONFLICT DO NOTHING',
-          [specialWeapon.id, specialWeapon.key],
-        ));
+      Promise.all(tasks)
+        .then(() => {
+          next();
+          console.log('_addWeaponClasses is successfully done.');
+        })
+        .catch(err => next(err));
+    },
+    _addSpecialWeapons(next) {
+      const tasks = specialWeapons.map(specialWeapon => db.raw(
+        'INSERT INTO special_weapons (special_weapon_id, special_weapon_key) VALUES (?, ?) ON CONFLICT DO NOTHING',
+        [specialWeapon.id, specialWeapon.key],
+      ));
 
-        Promise.all(tasks)
-          .then(() => {
-            next();
-            console.log('_addSpecialWeapons is successfully done.');
-          })
-          .catch(err => next(err));
-      },
-      _addSubWeapons(next) {
-        const tasks = subWeapons.map(subWeapon => db.raw(
-          'INSERT INTO sub_weapons (sub_weapon_id, sub_weapon_key) VALUES (?, ?) ON CONFLICT DO NOTHING',
-          [subWeapon.id, subWeapon.key],
-        ));
+      Promise.all(tasks)
+        .then(() => {
+          next();
+          console.log('_addSpecialWeapons is successfully done.');
+        })
+        .catch(err => next(err));
+    },
+    _addSubWeapons(next) {
+      const tasks = subWeapons.map(subWeapon => db.raw(
+        'INSERT INTO sub_weapons (sub_weapon_id, sub_weapon_key) VALUES (?, ?) ON CONFLICT DO NOTHING',
+        [subWeapon.id, subWeapon.key],
+      ));
 
-        Promise.all(tasks)
-          .then(() => {
-            next();
-            console.log('_addSubWeapons is successfully done.');
-          })
-          .catch(err => next(err));
-      },
-      _addStages(next) {
-        const tasks = stages.map(stage => db.raw(
-          'INSERT INTO stages (stage_id, stage_key) VALUES (?, ?) ON CONFLICT DO NOTHING',
-          [stage.id, stage.key],
-        ));
+      Promise.all(tasks)
+        .then(() => {
+          next();
+          console.log('_addSubWeapons is successfully done.');
+        })
+        .catch(err => next(err));
+    },
+    _addStages(next) {
+      const tasks = stages.map(stage => db.raw(
+        'INSERT INTO stages (stage_id, stage_key) VALUES (?, ?) ON CONFLICT DO NOTHING',
+        [stage.id, stage.key],
+      ));
 
-        Promise.all(tasks)
-          .then(() => {
-            next();
-            console.log('_addStages is successfully done.');
-          })
-          .catch(err => next(err));
-      },
-      _addRankedRules(next) {
-        const tasks = rankedRules.map(rule => db.raw(
-          'INSERT INTO ranked_rules (rule_id, rule_key) VALUES (?, ?) ON CONFLICT DO NOTHING',
-          [rule.id, rule.key],
-        ));
+      Promise.all(tasks)
+        .then(() => {
+          next();
+          console.log('_addStages is successfully done.');
+        })
+        .catch(err => next(err));
+    },
+    _addRankedRules(next) {
+      const tasks = rankedRules.map(rule => db.raw(
+        'INSERT INTO ranked_rules (rule_id, rule_key) VALUES (?, ?) ON CONFLICT DO NOTHING',
+        [rule.id, rule.key],
+      ));
 
-        Promise.all(tasks)
-          .then(() => {
-            next();
-            console.log('_addRankedRules is successfully done.');
-          })
-          .catch(err => next(err));
-      },
-      _addPastLeagueStages(next) {
-        // Convert spla2.yuu26.com format to splatoon2.ink format
-        const convertScheduleFormat = (_schedule) => {
-          const schedule = {};
-          schedule.stage_a = { id: _schedule.maps_ex[0].id };
-          schedule.stage_b = { id: _schedule.maps_ex[1].id };
-          schedule.rule = { key: _schedule.rule_ex.key };
-          schedule.start_time = _schedule.start_t;
-          return schedule;
-        };
+      Promise.all(tasks)
+        .then(() => {
+          next();
+          console.log('_addRankedRules is successfully done.');
+        })
+        .catch(err => next(err));
+    },
+    _addPastLeagueStages(next) {
+      // Convert spla2.yuu26.com format to splatoon2.ink format
+      const convertScheduleFormat = (_schedule) => {
+        const schedule = {};
+        schedule.stage_a = { id: _schedule.maps_ex[0].id };
+        schedule.stage_b = { id: _schedule.maps_ex[1].id };
+        schedule.rule = { key: _schedule.rule_ex.key };
+        schedule.start_time = _schedule.start_t;
+        return schedule;
+      };
 
-        fetch('https://spla2.yuu26.com/league', { headers: { 'User-Agent': config.THIRDPARTY_API_USERAGENT } })
-          .then(res => res.json())
-          .then((schedules) => {
-            db.transaction((trx) => {
-              const tasks = schedules.result.map((_schedule) => {
-                const schedule = convertScheduleFormat(_schedule);
-                const stageIds = [schedule.stage_a.id, schedule.stage_b.id];
-                return trx.raw(`
-                  INSERT
-                    INTO league_schedules (start_time, rule_id, stage_ids)
-                    VALUES (to_timestamp(?), ?, ?) ON CONFLICT DO NOTHING`,
-                [schedule.start_time, findRuleId(schedule.rule.key), stageIds]);
-              });
-              Promise.all(tasks)
-                .then(() => {
-                  trx.commit();
-                  console.log('_addPastLeagueStages is successfully done.');
-                  next();
-                })
-                .catch((err) => {
-                  trx.rollback();
-                  next(err);
-                });
+      fetch('https://spla2.yuu26.com/league', { headers: { 'User-Agent': config.THIRDPARTY_API_USERAGENT } })
+        .then(res => res.json())
+        .then((schedules) => {
+          db.transaction((trx) => {
+            const tasks = schedules.result.map((_schedule) => {
+              const schedule = convertScheduleFormat(_schedule);
+              const stageIds = [schedule.stage_a.id, schedule.stage_b.id];
+              return trx.raw(`
+                INSERT
+                  INTO league_schedules (start_time, rule_id, stage_ids)
+                  VALUES (to_timestamp(?), ?, ?) ON CONFLICT DO NOTHING`,
+              [schedule.start_time, findRuleId(schedule.rule.key), stageIds]);
             });
+            Promise.all(tasks)
+              .then(() => {
+                trx.commit();
+                console.log('_addPastLeagueStages is successfully done.');
+                next();
+              })
+              .catch((err) => {
+                trx.rollback();
+                next(err);
+              });
           });
-      },
-      _addWeapons(next) {
-        (async function () { // eslint-disable-line func-names
-          if (statInkWeapons) {
-            return statInkWeapons;
-          }
-
-          const res = await fetch('https://stat.ink/api/v2/weapon',
-            { headers: { 'User-Agent': config.THIRDPARTY_API_USERAGENT } });
-          return res.json();
-        }())
-          .then(weapons => addWeapons(weapons))
-          .then(() => {
-            console.log('_addWeapons is successfully done.');
-            next();
-          })
-          .catch(err => next(err));
-      },
+        });
     },
-    (err) => {
-      db.destroy();
+    _addWeapons(next) {
+      (async function () { // eslint-disable-line func-names
+        if (statInkWeapons) {
+          return statInkWeapons;
+        }
 
-      if (err) {
-        throw new Error(err);
-      }
-
-      console.log('Successfully populated database.');
+        const res = await fetch('https://stat.ink/api/v2/weapon',
+          { headers: { 'User-Agent': config.THIRDPARTY_API_USERAGENT } });
+        return res.json();
+      }())
+        .then(weapons => addWeapons(weapons))
+        .then(() => {
+          console.log('_addWeapons is successfully done.');
+          next();
+        })
+        .catch(err => next(err));
     },
-  );
+  },
+  (err) => {
+    db.destroy();
+
+    if (err) {
+      throw new Error(err);
+    }
+
+    console.log('Successfully populated database.');
+  });
 };
 
 module.exports = { populateDatabase };
