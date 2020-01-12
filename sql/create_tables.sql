cc7d69512cbe774007c87790a013676835c8bae6
yukidaruma <yukidaruma@users.noreply.github.com>
2020-01-12 19:20:56 +0900

    Rename index `league_rankings_league_id_idx` to `league_rankings_player_id_idx`

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS league_rankings (
   PRIMARY KEY (start_time, group_id, player_id)
 );
 -- CREATE INDEX IF NOT EXISTS league_rankings_group_type_idx ON league_rankings (group_type);
-CREATE INDEX IF NOT EXISTS league_rankings_league_id_idx ON league_rankings (player_id);
+CREATE INDEX IF NOT EXISTS league_rankings_player_id_idx ON league_rankings (player_id);
 
 -- This table is used to prevent fetch-league-rankings from fetching nonexistent league rankings.
 CREATE TABLE IF NOT EXISTS missing_league_rankings (
