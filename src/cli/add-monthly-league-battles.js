a1fd05f5180f75e5137e5981573b4161a4a6e22f
yukidaruma <yukidaruma@users.noreply.github.com>
2020-04-26 01:45:49 +0900

    Add CLI command `add-monthly-league-battles`

---
+++ src/cli/add-monthly-league-battles.js
@@ -0,0 +1,24 @@
+const commandLineArgs = require('command-line-args');
+const { db } = require('../db');
+const { dateToSqlTimestamp } = require('../util');
+
+/** @type {{ startTimes: string[] }} */
+const { startTimes } = commandLineArgs([
+  { name: 'startTimes', multiple: true, defaultOption: true },
+]);
+
+db.transaction(async (trx) => {
+  try {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const startTime of startTimes) {
+      // eslint-disable-next-line no-await-in-loop
+      await db('monthly_league_battle_schedules')
+        .transacting(trx)
+        .insert({ start_time: dateToSqlTimestamp(startTime) });
+    }
+
+    await trx.commit();
+  } catch (e) {
+    await trx.rollback(e);
+  }
+}).finally(() => db.destroy());
