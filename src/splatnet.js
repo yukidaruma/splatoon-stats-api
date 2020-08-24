ded6319b5f698132720bace4de933c2951783058
yukidaruma <yukidaruma@users.noreply.github.com>
2020-08-24 16:04:48 +0900

    Remove `request` from dependencies as it's deprecated
    Close #7

--- src/splatnet.js
+++ src/splatnet.js
@@ -1,37 +1,39 @@
 // This code is based on @hymm's SquidTracks (Released under the MIT License)
 // https://github.com/hymm/squid-tracks/blob/ccc63e82dec32191a6d94f14d77b34b9932b5b0b/public/main/splatnet2.js
-const Request = require('request-promise-native');
+
+const http = require('https');
+const { default: fetch } = require('node-fetch');
+
 const config = require('../config');
 const { NintendoAPIError } = require('./errors');
 
 const splatnetUrl = 'https://app.splatoon2.nintendo.net';
 
-const jar = Request.jar();
-const request = Request.defaults({ jar });
-const cookie = request.cookie(`iksm_session=${config.IKSM_SESSION}`);
-jar.setCookie(cookie, splatnetUrl);
+const Cookie = `iksm_session=${config.IKSM_SESSION}`;
 
-const getSplatnetApi = path => new Promise((resolve, reject) => {
+const getSplatnetApi = async (path) => {
   const uri = `${splatnetUrl}/api/${path}`;
-  request({
+
+  const res = await fetch(uri, {
+    agent: new http.Agent({
+      keepAlive: true,
+    }),
     method: 'GET',
-    uri,
     headers: {
-      Accept: '*/*',
-      'Accept-Encoding': 'gzip, deflate',
       'Accept-Language': config.USER_LANGUAGE,
       'User-Agent': config.NINTENDO_API_USERAGENT,
+      Cookie,
       Connection: 'keep-alive',
     },
-    json: true,
-    gzip: true,
-  })
-    .then(res => resolve(res))
-    .catch((err) => {
-      const nintendoApiError = new NintendoAPIError(`Request to ${uri} failed. Status code: ${err.statusCode}`);
-      nintendoApiError.statusCode = err.statusCode;
-      return reject(nintendoApiError);
-    });
-});
+  });
+
+  if (res.ok) {
+    return res.json();
+  }
+
+  const nintendoApiError = new NintendoAPIError(`Request to ${uri} failed. Status code: ${res.status}`);
+  nintendoApiError.statusCode = res.status;
+  throw nintendoApiError;
+};
 
 module.exports = { splatnetUrl, getSplatnetApi };
