1ca0205361eff8f3dd688de6abb0d1e75add7009
yukidaruma <yukidaruma@users.noreply.github.com>
2020-12-13 18:23:56 +0900

    chore: Revert removal of npm run script `update-locale`, as it is used by scheduled Tweet
    `ui-localization.json` is completely removed though.
    
    Reverted commits:
    4468c5193e0739ce24d693bdf39588c78e63ea22
    18b360a9306f26220fb1df08b9bc66b707e19ca1

--- src/cli/setup.js
+++ src/cli/setup.js
@@ -1,8 +1,37 @@
 const fetch = require('node-fetch');
+const fs = require('fs');
 const commandLineArgs = require('command-line-args');
 const config = require('../../config');
 const { populateDatabase } = require('../populate_database');
 const { cacheImageFromNintendoAPI } = require('../cron-job');
+
+const downloadLocales = (statInkWeapons) => {
+  const languages = ['ja', 'en'];
+  languages.forEach(async (lang) => {
+    const locale = { ja: 'ja_JP', en: 'en_US' }[lang];
+    const cacheDir = 'cache/locale';
+
+    if (!fs.existsSync(cacheDir)) {
+      fs.mkdirSync(cacheDir, { recursive: true });
+    }
+
+    const cachePath = `${cacheDir}/${lang}.json`;
+    if (!fs.existsSync(cachePath)) {
+      const res = await fetch(`https://splatoon2.ink/data/locale/${lang}.json`,
+        { headers: { 'User-Agent': config.THIRDPARTY_API_USERAGENT } });
+      const localeData = await res.json();
+
+      // Find and complete missing translations
+      statInkWeapons
+        .filter(weapon => !localeData.weapons[weapon.splatnet])
+        .forEach((weapon) => {
+          localeData.weapons[weapon.splatnet] = { name: weapon.name[locale] };
+        });
+
+      fs.writeFileSync(cachePath, JSON.stringify(localeData));
+    }
+  });
+};
 
 const fetchSalmonAssets = () => {
   const assetClasses = {
@@ -40,6 +69,11 @@ const fetchSalmonAssets = () => {
   // c.f. https://github.com/75lb/command-line-args/issues/71
   const options = commandLineArgs([
     {
+      name: 'no-locale',
+      type: Boolean,
+      defaultValue: false,
+    },
+    {
       name: 'no-database',
       type: Boolean,
       defaultValue: false,
@@ -54,6 +88,10 @@ const fetchSalmonAssets = () => {
   const res = await fetch('https://stat.ink/api/v2/weapon',
     { headers: { 'User-Agent': config.THIRDPARTY_API_USERAGENT } });
   const statInkWeapons = await res.json();
+
+  if (!options['no-locale']) {
+    downloadLocales(statInkWeapons);
+  }
 
   if (!options['no-database']) {
     populateDatabase(statInkWeapons);
